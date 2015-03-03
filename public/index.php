79df966843525a661eb98ec91c1937b3c3e29032
Corentin Larose <corentin.larose@qapa.com>
2015-03-03 23:24:44 +0100

    Notes + index

--- public/index.php
+++ public/index.php
@@ -1,26 +1,37 @@
 <?php
-if (getenv('APPLICATION_ENV')) {
-    define('APPLICATION_ENV', getenv('APPLICATION_ENV'));
-} else {
-    define('APPLICATION_ENV', 'testing');
-}
+call_user_func(function () {
+    if (getenv('APPLICATION_ENV')) {
+        define('APPLICATION_ENV', getenv('APPLICATION_ENV'));
+    } else {
+        define('APPLICATION_ENV', 'testing');
+    }
 
-$locale = \Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-\Locale::setDefault($locale);
+    $locale = \Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    \Locale::setDefault($locale);
 
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
+    /**
+     * This makes our life easier when dealing with paths. Everything is relative
+     * to the application root now.
+     */
+    chdir(dirname(__DIR__));
 
-// Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false;
-}
+    // Decline static file requests back to the PHP built-in webserver
+    if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+        return false;
+    }
 
-// Setup autoloading
-require 'init_autoloader.php';
+    // Setup autoloading
+    require 'init_autoloader.php';
 
-// Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+    $appConfig = include 'config/application.config.php';
+
+    if (is_file('config/development.config.php')) {
+        $appConfig = \Zend\Stdlib\ArrayUtils::merge(
+            $appConfig,
+            include 'config/development.config.php'
+        );
+    }
+
+    // Run the application!
+    Zend\Mvc\Application::init($appConfig)->run();
+});
